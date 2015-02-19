a7d0f110c01188e405c59f4e647752b1ceb5647b
Olivier Vincent <Vincent.O@outlook.com>
2015-02-18 20:20:52 -0500

    Modification de SystemeSecurite
    utilisation de .size() pour la taille des vecteurs

--- INF1010/TP3/SystemeSecurite.cpp
+++ INF1010/TP3/SystemeSecurite.cpp
@@ -15,8 +15,7 @@ bool SystemeSecurite::accederLocal(const AgentSecurite& agent, const string& loc
 	stringstream ss;
 	char niveauAccesString[3];
 	sprintf(niveauAccesString, "%d", agent.getNiveauAcces());
-	int tailleVecteur = (regles_.end() - regles_.begin());
-	for (int i = 0; i < tailleVecteur; i++){
+	for (int i = 0; i < regles_.size(); i++){
 		if ((regles_[i].getLocal() == local) &&
 			(regles_[i].getPeriode() == periode) &&
 			(regles_[i].getNiveauAccesRequis() >= agent.getNiveauAcces())){
@@ -32,8 +31,7 @@ bool SystemeSecurite::accederLocal(const Professeur& prof, const string& local, 
 	bool estAccepte = false;
 	char niveauAccesString[3];
 	sprintf(niveauAccesString, "%d", prof.getNiveauAcces());
-	int tailleVecteur = (regles_.end() - regles_.begin());
-	for (int i = 0; i < tailleVecteur; i++){
+	for (int i = 0; i < regles_.size(); i++){
 		if ((regles_[i].getLocal() == local) && 
 			(regles_[i].getPeriode() == periode) && 
 			(regles_[i].getNiveauAccesRequis() >= prof.getNiveauAcces())){
@@ -49,8 +47,7 @@ bool SystemeSecurite::accederLocal(const Etudiant& etudiant, const string& local
 	bool estAccepte = false;
 	char niveauAccesString[3];
 	sprintf(niveauAccesString, "%d", etudiant.getNiveauAcces());
-	int tailleVecteur = (regles_.end() - regles_.begin());
-	for (int i = 0; i < tailleVecteur; i++){
+	for (int i = 0; i < regles_.size(); i++){
 		if ((regles_[i].getLocal() == local) &&
 			(regles_[i].getPeriode() == periode) &&
 			(regles_[i].getNiveauAccesRequis() >= etudiant.getNiveauAcces())){
@@ -66,8 +63,7 @@ bool SystemeSecurite::accederLocal(const string& nom, const string& prenom, cons
 	bool estAccepte = false;
 	char niveauAccesString[3];
 	sprintf(niveauAccesString, "%d", niveauAcces);
-	int tailleVecteur = (regles_.end() - regles_.begin());
-	for (int i = 0; i < tailleVecteur; i++){
+	for (int i = 0; i < regles_.size(); i++){
 		if ((regles_[i].getLocal() == local) &&
 			(regles_[i].getPeriode() == periode) &&
 			(regles_[i].getNiveauAccesRequis() >= niveauAcces)){
@@ -82,8 +78,7 @@ bool SystemeSecurite::accederLocal(const string& nom, const string& prenom, cons
 bool SystemeSecurite::ajouterRegle(const RegleAcces& regle)
 { 
 	bool estPresente = false;
-	int tailleVecteur = (regles_.end() - regles_.begin());
-	for (int i = 0; i < tailleVecteur; i++){
+	for (int i = 0; i < regles_.size(); i++){
 		if ((regles_.at(i).getLocal() == regle.getLocal()) && (regles_.at(i).getPeriode() == regle.getPeriode()) && (regles_.at(i).getNiveauAccesRequis() == regle.getNiveauAccesRequis())){
 			estPresente = true;
 		}
@@ -96,16 +91,16 @@ bool SystemeSecurite::ajouterRegle(const RegleAcces& regle)
 
 void SystemeSecurite::imprimerJournal() const
 {
-	int tailleVecteur = (journalAcces_.end() - journalAcces_.begin());
-	for (int i = 0; i < tailleVecteur; i++){
+	for (int i = 0; i < regles_.size(); i++){
 		cout << journalAcces_[i];
 	}
 }
 
-string SystemeSecurite :: formatAffichage(const string& nom, const string& prenom, const string& fonction, unsigned int niveauAcces, const string& local, const string& periode, const bool estAccede){
+string SystemeSecurite :: formatAffichage(const string& nom, const string& prenom, const string& fonction, unsigned int niveauAcces, const string& local, const string& periode, const bool estAccede)
+{
 	stringstream ss;
 	if (estAccede){
-		ss << "Nom, Prenom:" << nom << ", " << prenom << endl
+		ss << "Nom, Prenom: " << nom << ", " << prenom << endl
 			<< "Classe d'employe: " << fonction << endl
 			<< "Niveau d'acces: " << niveauAcces << endl
 			<< "Local: " << local << endl
@@ -113,7 +108,7 @@ string SystemeSecurite :: formatAffichage(const string& nom, const string& preno
 			<< "Acces: Accorde" << endl;
 	}
 	else{
-		ss << "Nom, Prenom:" << nom << ", " << prenom << endl
+		ss << "Nom, Prenom: " << nom << ", " << prenom << endl
 			<< "Classe d'employe: " << fonction << endl
 			<< "Niveau d'acces: " << niveauAcces << endl
 			<< "Local: " << local << endl
