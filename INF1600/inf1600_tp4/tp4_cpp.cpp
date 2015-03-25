e2d5e470ea910242b74ff5b238883e6a91047c62
Jérémie Poisson <jeremie@weboxigen.com>
2015-03-24 20:41:33 -0400

    Added missing method implementation for canevas

--- INF1600/inf1600_tp4/tp4_cpp.cpp
+++ INF1600/inf1600_tp4/tp4_cpp.cpp
@@ -14,31 +14,36 @@ void ecrireFrame(const Canevas* c);
 
 
 Cercle* nouveauCercle(Coord centre, unsigned int rayon, unsigned int couleur) {
-	Cercle* nouveau = new Cercle(cnetre, rayon, couleur);
+	Cercle* nouveau = new Cercle(centre, rayon, couleur); // dont forget to explicitly call the constructor in assemble + the new operator
 	return nouveau; // A modifier
 }
 
 void faireDessin(Forme** formes, unsigned int n) {
 	Canevas canevas(LARGEUR_CANEVAS, HAUTEUR_CANEVAS, COULEUR_CANEVAS);
 	for (unsigned int i = 0; i < n; i++)
+	{
 		canevas.dessinerForme(formes[i]);
-		
+	}
+
 	ecrireFrame(&canevas);
 }
 
 
 void desallouerForme(Forme* forme) {
-
+	delete forme; // dont forget to explicitly call the destructor in assembly
 }
 
 void Canevas::dessinerForme(Forme *forme)
 {
-	forme -> dessiner(this);
+	forme->dessiner(this);
 }
 
 void Canevas::assignerPixel(int x, int y, unsigned int couleur)
 {
-
+	if(x > 0 && x < (int)this->_w && y > 0 && y < (int)this->_h)
+	{
+		this->_canevas[y * this->_w + x] = couleur;
+	}
 }
 
 void Canevas::assignerPixel(Coord coord, unsigned int couleur)
